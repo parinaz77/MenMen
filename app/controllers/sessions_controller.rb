423fcc41fe8b03c19f244f49054779372e1712a8
Dawit Hailu <dawithailu@Dawits-MacBook-Pro.local>
2016-12-08 12:04:13 -0800

    made phhilip be able to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to user_path(user)
     else
-      redirect_to '/signup'
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to sessions_path
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  
 
 end
