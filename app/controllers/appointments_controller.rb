d74f438838681d3c14734aa7491c1a946779b3a1
vic6 <vicmatthews116@gmail.com>
2016-12-08 15:02:33 -0800

    Work on appointment contorller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,1 +1,14 @@
-appointments_controller.rb
+class AppointmentsController < ApplicationController
+  def index
+  end
+
+  def show
+    @appointments = current_user.metor_sessions.find(params[:appointment_id])
+  end
+
+  # private
+
+  # def comment_params
+  #   params.require(:appointment).permit(:commenter, :body)
+  # end
+end
