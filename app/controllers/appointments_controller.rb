04de799bea77ae40dcd490b66bdf067619e7dc8a
Dawit Hailu <dawithailu@Dawits-MacBook-Pro.local>
2016-12-08 19:42:27 -0800

    add route for mentore to add appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,14 +5,25 @@ class AppointmentsController < ApplicationController
   def show
     @user = User.find(params[:id])
     p "=" * 90
-    p @user.user_type
-    p @user.study_sessions.first.topic
+ 
     if @user.user_type == "mentor"
      @appointment = @user.mentor_sessions.find(params[:user_id])
    else
    	@appointment = @user.study_sessions.find(params[:user_id])
    end
   end
+
+   def create
+   
+
+   args = params[:appointment]
+   args[:mentor_id] = params[:user_id]
+   
+
+   a = Appointment.create(appointment_params)
+   
+  redirect_to user_appointment_path(@appointment)
+ end
 
   # private
 
