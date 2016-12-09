f327c42c784298d7a72fc70d7bdfdfb043fac3bf
vic6 <vicmatthews116@gmail.com>
2016-12-08 19:29:37 -0800

    Appointments are not working

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,18 +2,32 @@ class AppointmentsController < ApplicationController
   def index
   end
 
+  def new
+    @appointment = Appointment.new
+  end
+
   def show
     @user = User.find(params[:id])
-    p "=" * 90
-    p @user.user_type
-    p @user.study_sessions.first.topic
     if @user.user_type == "mentor"
      @appointment = @user.mentor_sessions.find(params[:user_id])
    else
-   	@appointment = @user.study_sessions.find(params[:user_id])
+    @appointment = @user.study_sessions.find(params[:user_id])
    end
   end
 
+
+  def create
+    
+
+    args = params[:appointment]
+    args[:mentor_id] = params[:user_id]
+    
+
+    a = User.new_appointment(args)
+    p "=" * 88
+    p a
+   redirect_to user_appointment_path(@appointment)
+  end
   # private
 
   # def comment_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ActionController::Base
 	before_action :user_params, only: [:create]
 
 	def new
-		session = nil
 		@user = User.new
 	end
 
