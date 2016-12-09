9ce6050166bd9c21e971fda1f1a2359a39bb05bf
Jason Warholic <warholic.jason@gmail.com>
2016-12-08 17:36:57 -0800

    update link for logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   end
 
   resources :sessions, only: [:new, :create, :destroy]
-  delete '/logout', to: 'sessions#destroy' 
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
   
 end
