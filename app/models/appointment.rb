9b8c16ffd495d12ec2e3807b97cc27b3b548f063
parinaz77 <parinaz77@gmail.com>
2016-12-09 14:58:17 -0800

    add upcoming and past appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,4 +4,13 @@ class Appointment < ActiveRecord::Base
   belongs_to :student, class_name: "User"
 
   validates :start_time, :topic, presence: true
+
+  validate :time_is_in_future
+
+  def time_is_in_future
+    if start_time < Time.now
+      @errors.add(:start_time, "start_time must be in the future")
+    end
+  end
+
 end
